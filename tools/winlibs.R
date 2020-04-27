8d9e311d60a607e51deb59ba40d8c85edef415b4
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2020-04-27 23:47:39 +0200

    fix build on win

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -9,3 +9,13 @@ if(!file.exists(sprintf("../windows/opencv-%s/include/opencv4/opencv2/opencv.hpp
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
 }
+
+# Build against imagemagick static website.
+MAG_IM <- 6
+MAG_VERSION <- "6.9.9"
+if(!file.exists(sprintf("../windows/imagemagick%s-%s/include/ImageMagick-%s/Magick++.h", MAG_IM, MAG_VERSION, MAG_IM))){
+  curl::curl_download(sprintf("https://github.com/rwinlib/imagemagick%s/archive/v%s.zip", MAG_IM, MAG_VERSION), "lib.zip")
+  dir.create("../windows", showWarnings = FALSE)
+  unzip("lib.zip", exdir = "../windows")
+  unlink("lib.zip")
+}
